2d4d5da0591073c648c9930d0ad4c5964fa4605b
Kim Jeonghwan <ej88ej@gmail.com>
2020-01-11 13:18:23 +0900

    바벨 실습용

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path = require("path");
+const webpack = require("webpack");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 module.exports = {
   mode: "development",
@@ -13,7 +20,12 @@ module.exports = {
     rules: [
       {
         test: /\.(scss|css)$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader // 프로덕션 환경
+            : "style-loader", // 개발 환경
+          "css-loader"
+        ]
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
@@ -23,13 +35,32 @@ module.exports = {
           limit: 10000 // 10Kb
         }
       }
+      /**
+       * TODO: babel-loader를 구성해 보세요.
+       */
     ]
-  }
-  /**
-   * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
-   * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
-   * 2. HtmlTemplatePlugin: 동적으로 html 파일을 생성하세요.
-   * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
-   * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
-   */
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true // 주석 제거
+            }
+          : false,
+      hash: process.env.NODE_ENV === "production"
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : [])
+  ]
 };

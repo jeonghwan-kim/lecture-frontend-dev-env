9957998f754e2bdca8e629205b331fcda6c4689b
Kim Jeonghwan <ej88ej@gmail.com>
2020-01-11 14:48:50 +0900

    웹팩개발서버 실습용

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,6 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const apiMocker = require("connect-api-mocker");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 
 const mode = process.env.NODE_ENV || "development";
 
@@ -21,14 +17,8 @@ module.exports = {
   },
   devServer: {
     overlay: true,
-    stats: "errors-only",
-    // before: (app, server, compiler) => {
-    //   app.use(apiMocker('/api', 'mocks/api'));
-    // },
-    proxy: {
-      "/api": "http://localhost:8081"
-    },
-    hot: true
+    stats: "errors-only"
+    // TODO: 여기에 api 서버 프록싱 설정을 추가하세요
   },
   module: {
     rules: [
@@ -77,32 +67,8 @@ module.exports = {
       hash: mode === "production"
     }),
     new CleanWebpackPlugin(),
-    new CopyPlugin([
-      {
-        from: "./node_modules/axios/dist/axios.min.js",
-        to: "./axios.min.js" // 목적지 파일에 들어간다
-      }
-    ]),
     ...(mode === "production"
       ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
       : [])
-  ],
-  optimization: {
-    minimizer:
-      mode === "production"
-        ? [
-            new OptimizeCSSAssetsPlugin(),
-            new TerserPlugin({
-              terserOptions: {
-                compress: {
-                  drop_console: true // 콘솔 로그를 제거한다
-                }
-              }
-            })
-          ]
-        : []
-  },
-  externals: {
-    axios: "axios"
-  }
+  ]
 };

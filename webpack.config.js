93fbdc0f178f32f037d7c9b634a8e35c04d4d95c
Kim Jeonghwan <ej88ej@gmail.com>
2020-02-11 20:30:55 +0900

    오타수정: HtmlTemplatePlugin -> HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   /**
    * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
    * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
-   * 2. HtmlTemplatePlugin: 동적으로 html 파일을 생성하세요.
+   * 2. HtmlWebpackPlugin: 동적으로 html 파일을 생성하세요.
    * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
    * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
    */

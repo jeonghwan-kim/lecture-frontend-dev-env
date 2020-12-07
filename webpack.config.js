94331bc7183509e70c52765575cb43f1c2c55f94
GoldK11 <ssori113@gmail.com>
2020-12-07 17:54:39 +0900

    practice01

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: "./src/app.js"
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve("./dist")
+    }
+}

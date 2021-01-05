dc9450c7d577db7ad5e8f01537d8c6d1624a7fcd
아이슥 <leesak@aiseug-ui-MacBookPro.local>
2021-01-05 15:42:37 +0900

    webpack css complete 웹팩 css로더, 스타일 로더, 파일로더 webpack config 파일에 적용 완료

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import MainController from "./controllers/MainController.js";
+import "./main.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   new MainController();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/app.js'
+    main: "./src/app.js",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  }
-}
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // .css 확장자로 끝나는 모든 파일
+        use: ["style-loader", "css-loader"], // css-loader를 적용한다
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: {
+          loader: "url-loader", // url 로더를 설정한다
+          options: {
+            publicPath: "./dist/", // file-loader와 동일
+            name: "[name].[ext]?[hash]", // file-loader와 동일
+            limit: 10000, // 10kb 미만 파일만 data url로 처리
+          },
+        },
+      },
+    ],
+  },
+};

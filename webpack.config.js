6078bdb6d98fe3539b6540c60f6528919503d860
salybu <rkdmsdl93@gmail.com>
2021-12-30 21:37:37 +0900

    ✅ entry / output 실습

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+};
